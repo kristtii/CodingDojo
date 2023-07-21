0f1b9bd11e6863fa97b3a9207981dd04595615fd
kristtii <kristizoto9@gmail.com>
2023-07-21 07:25:43 +0200

    Small fix

--- Web Fundamentals/likes/script.js
+++ Web Fundamentals/likes/script.js
@@ -1,23 +1,6 @@
-let count_1 = 9;
-likesElement_1 = document.querySelector("#post-1")
-
-function addLikes(){
-    count_1 += 1;
-    likesElement_1.innerText = count_1 + " like(s)";
-}
-
-let count_2 = 12;
-likesElement_2 = document.querySelector("#post-2")
-
-function addLikes2(){
-    count_2 += 1;
-    likesElement_2.innerText = count_2 + " like(s)";
-}
-
-let count_3 = 9;
-likesElement_3 = document.querySelector("#post-3")
-
-function addLikes3(){
-    count_3 += 1;
-    likesElement_3.innerText = count_3 + " like(s)";
+function addLikes(postNumber) {
+    const likesElement = document.getElementById(`post-${postNumber}`);
+    const currentLikes = parseInt(likesElement.textContent);
+    const newLikes = currentLikes + 1;
+    likesElement.textContent = `${newLikes} like(s)`;
 }
