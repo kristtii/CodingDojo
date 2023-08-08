eef3907fb7b604ff37dee03176585ce12bd58d79
kristtii <kristizoto9@gmail.com>
2023-08-08 08:49:58 +0200

    Functions Intermediate 1

---
+++ Python/functions.py
@@ -0,0 +1,80 @@
+x = [[5, 2, 3], [10, 8, 9]]
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'}
+]
+sports_directory = {
+    'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer': ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [{'x': 10, 'y': 20}]
+
+x[1][0] = 15
+print(x)
+
+sports_directory['basketball'][1] = "Bryant"
+print(sports_directory)
+
+sports_directory['soccer'][0] = "Andres"
+print(sports_directory)
+
+z[0]['y'] = 30
+print(z)
+
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+# # should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# # bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+
+def iterateDictionary(dict):
+    for element in dict:
+        output = ""
+        for key, value in element.items():
+            output += f"{key} - {value}, "
+        print(output)
+
+
+iterateDictionary(students)
+
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+
+def iterateDictionary2(key_name, some_list):
+    for values in some_list:
+        for k, value in values.items():
+            if k == key_name:
+                print(values[k])
+
+
+iterateDictionary2("first_name", students)
+iterateDictionary2("last_name", students)
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+
+def printInfo(dict):
+    for key, value in dict.items():
+        print(len(value), key.upper())
+        for i in value:
+            print(i)
+
+
+printInfo(dojo)
